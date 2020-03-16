c311c9b3b283c617cbac956bbd6465d800a1cd37
Half-Engg <62250167+Half-Engg@users.noreply.github.com>
2020-03-16 19:56:44 +0530

    publishing my first website
    wow.....

---
+++ JS/card.js
@@ -0,0 +1,9 @@
+/*====================================
+                    Preloader
+======================================*/
+
+$(window).on('load', function () //makes sure that the whole site is loaded
+    {
+        $('#status').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+    });
